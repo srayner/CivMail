bac52157524aa4aaef7d1e60ee1b3a76fa70ab82
Steve Rayner <srayner02@gmail.com>
2016-05-27 14:09:28 +0100

    Modified service.

--- src/CivMail/service/MailService.php
+++ src/CivMail/service/MailService.php
@@ -5,6 +5,8 @@ namespace CivMail\Service;
 use Zend\Mail;
 use Zend\Mail\Transport\Smtp as SmtpTransport;
 use Zend\Mail\Transport\SmtpOptions;
+use Zend\Mime\Message as MimeMessage;
+use Zend\Mime\Part as MimePart;
 
 class MailService
 {
@@ -27,5 +29,34 @@ class MailService
         
         $transport->setOptions(new SmtpOptions($this->options));
         $transport->send($message);
+    }
+    
+    public function sendMessage(Array $mail)
+    {
+        // Build the message body
+        $parts = $mail['parts'];
+        $mimeParts = array();
+        foreach($parts as $part) {
+            $mimePart = new MimePart($part['content']);
+            $mimePart->type = $part['type'];
+            $mimeParts[] = $part;
+        }
+        $body = new MimeMessage();
+        $body->setParts($mimeParts);
+        
+        // Build the message.
+        $message = new  Mail\Message();
+        $message->setBody($body);
+        $message->setFrom($mail['from']);
+        $message->addTo($mail['to']);
+        $message->setSubject($mail['subject']);
+
+        // Create the transport and send.
+        $transport = new SmtpTransport();
+        $transport->setOptions(new SmtpOptions($this->options));
+        $transport->send($message);
+        
+        die(var_dump($mimeParts));
+        
     }
 }
